41b8a856e410d6a30f2c503ba6a87dd3992e7917
Victor Ciolac <victor.bcfraga@gmail.com>
2024-01-25 19:10:51 -0300

    Req.5

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,3 +1,11 @@
 def find_duplicate(nums):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    seen = set()
+
+    for num in nums:
+        if not isinstance(num, int) or num < 0:
+            return False
+        if num in seen:
+            return num
+        seen.add(num)
+
+    return False
