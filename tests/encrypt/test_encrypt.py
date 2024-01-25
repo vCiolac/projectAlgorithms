6c5e1770e1d3644f38720d11887b01b9bd1a85b3
Victor Ciolac <victor.bcfraga@gmail.com>
2024-01-25 17:32:00 -0300

    Req.2

--- tests/encrypt/test_encrypt.py
+++ tests/encrypt/test_encrypt.py
@@ -1,5 +1,19 @@
-# from challenges.challenge_encrypt_message import encrypt_message
+import pytest
+from challenges.challenge_encrypt_message import encrypt_message
 
 
 def test_encrypt_message():
-    pass
+    assert encrypt_message("TypeScript", 4) == "tpircS_epyT"
+    assert encrypt_message("Test", 1) == "T_tse"
+    assert encrypt_message("Pitanga", 3) == "tiP_agna"
+    assert encrypt_message("Crocodilo", 0) == "olidocorC"
+    assert encrypt_message("Trem", 7) == "merT"
+    assert encrypt_message("", 9) == ""
+
+    with pytest.raises(TypeError) as e:
+        encrypt_message(123, 2)
+    assert str(e.value) == "tipo inválido para message"
+
+    with pytest.raises(TypeError) as e:
+        encrypt_message("m4dr0g4d4", "ésolidao")
+    assert str(e.value) == "tipo inválido para key"
