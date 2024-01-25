81ec80de2ca528294c1b53a3f1ba0e7bf95f603a
Victor Ciolac <victor.bcfraga@gmail.com>
2024-01-25 19:16:18 -0300

    Req.6 - 100%

--- challenges/challenge_palindromes_iterative.py
+++ challenges/challenge_palindromes_iterative.py
@@ -1,3 +1,11 @@
 def is_palindrome_iterative(word):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    if not word:
+        return False
+    first_index = 0
+    last_index = len(word) - 1
+    while first_index < last_index:
+        if word[first_index] != word[last_index]:
+            return False
+        first_index += 1
+        last_index -= 1
+    return True
