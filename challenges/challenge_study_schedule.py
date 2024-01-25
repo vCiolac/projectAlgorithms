ff1e4002c57c90c6ec5131f860f829c4bd73839a
Victor Ciolac <victor.bcfraga@gmail.com>
2024-01-25 17:08:52 -0300

    Req.1 - tentei fazer sem for, chamando a propria func, tava mt dificil, fui simplificar e passou...

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,3 +1,9 @@
 def study_schedule(permanence_period, target_time):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    try:
+        count = 0
+        for period in permanence_period:
+            if period[0] <= target_time <= period[1]:
+                count += 1
+        return count
+    except TypeError:
+        return None
