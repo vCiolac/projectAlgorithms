e1d49d84e746d387d08d7681b6803b692d274978
Victor Ciolac <victor.bcfraga@gmail.com>
2024-01-25 18:55:52 -0300

    Req.4 - esse foi maneiro

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,3 +1,38 @@
 def is_anagram(first_string, second_string):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    f_sorted = merge_sort(first_string.lower())
+    s_sorted = merge_sort(second_string.lower())
+
+    f_join = ''.join(f_sorted)
+    s_join = ''.join(s_sorted)
+
+    if not first_string or not second_string:
+        if not first_string:
+            return ('', s_join, False)
+        else:
+            return (f_join, '', False)
+
+    return (f_join, s_join, f_sorted == s_sorted)
+
+
+def merge_sort(value):
+    if len(value) <= 1:
+        return value 
+    mid = len(value) // 2
+    left = merge_sort(value[:mid])
+    right = merge_sort(value[mid:])
+    return merge(left, right)
+
+
+def merge(left, right):
+    result = []
+    i = j = 0
+    while i < len(left) and j < len(right):
+        if left[i] <= right[j]:
+            result.append(left[i])
+            i += 1
+        else:
+            result.append(right[j])
+            j += 1
+    result.extend(left[i:])
+    result.extend(right[j:])
+    return result
